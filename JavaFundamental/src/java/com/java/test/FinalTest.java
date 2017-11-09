8ab624457b7725438cad992bf7f9041819c651af
njupt <18362971301@163.com>
2017-11-09 09:41:12 +0800

    Final关键字的使用

---
+++ JavaFundamental/src/java/com/java/test/FinalTest.java
@@ -0,0 +1,14 @@
+package com.java.test;
+
+/**
+ * final关键字的使用
+ * final关键字不能修饰interface,abstract类，因为这些类型是提供给
+ * 子类去继承，实现重写的，但是final修饰的类，方法任何都不能被修改
+ *
+ * final修饰变量，该变量是常量，有且只能赋值一次
+ *
+ * final修饰的方法能被继承，但是不能能被修改
+ * Created by 18362 on 2017/11/9.
+ */
+public class FinalTest {
+}
