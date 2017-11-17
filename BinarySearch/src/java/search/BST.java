08df46bf256dcca3068cccda54ef25427f0b31a3
njupt <18362971301@163.com>
2017-11-17 19:58:14 +0800

    二叉搜索树的建立，二叉搜索，contains函数实现

---
+++ BinarySearch/src/java/search/BST.java
@@ -0,0 +1,116 @@
+package search;
+
+/**
+ * 二叉搜索树的基本数据结构的构造
+ * Created by 18362 on 2017/11/17.
+ */
+public class BST<Key extends Comparable, Value extends Comparable> {
+    //树节点的构造
+    class Node {
+        private Key key;//按照键值排序
+        private Value value;//value的值
+        private Node leftnode;//左孩子
+        private Node rightnode;//右孩子
+
+        public Node(Key key, Value value) {
+            this.key = key;
+            this.value = value;
+            this.leftnode = this.rightnode = null;
+        }
+
+        public Node(Node node) {
+            this.key = node.key;
+            this.value = node.value;
+            this.leftnode = node.leftnode;
+            this.rightnode = node.rightnode;
+        }
+    }
+
+    public BST() {
+        root = null;
+        count = 0;
+    }
+
+    private Node root;//记录树的根节点
+    private int count;//记录节点的数据量
+
+    public int size() {
+        return count;
+    }
+
+    public Boolean IsEmpty() {
+        return count == 0;
+    }
+
+    //二叉搜索树的节点的插入，递归函数的使用方式,返回插入新的树节点之后的二叉树的根节点root！递归调用，明白了！
+    public void insert(Key key, Value value) {
+       root  = insert(root, key, value);
+    }
+    //根节点永远不会变化
+    private Node insert(Node root, Key key, Value value) {
+        if (root == null) {
+            count++;
+            return new Node(key, value);
+        }
+        if (key.compareTo(root.key) == 0) {
+            root.value = value;
+        } else if (key.compareTo(root.key) < 0)
+            root.leftnode = insert(root.leftnode, key, value);
+        else
+            root.rightnode = insert(root.rightnode, key, value);
+        return root;
+    }
+
+    //二叉搜索树的迭代实现,根节点一直在变化？？
+    public void insert2(Key key, Value value) {
+        if (root == null) {
+            root = new Node(key, value);
+            return;
+        }
+        Node node=root;
+        while (node != null) {
+            if (key.compareTo(node.key) == 0) {
+                node.value = value;
+                return;
+            } else if (key.compareTo(node.key) > 0) {
+                if (node.rightnode == null) {
+                    node.rightnode = new Node(key, value);
+                    return;
+                }
+                node = node.rightnode;
+            } else {
+                if (node.leftnode == null) {
+                    node.leftnode = new Node(key, value);
+                    return;
+                }
+                node = node.leftnode;
+            }
+        }
+    }
+    //contains函数的实现，是否包含某一个键值
+   public  Boolean contains(Node root,Key key)
+   {
+       if(root==null)
+           return false;//递归的出口
+       if(key.compareTo(root.key)==0)
+           return true;
+       else if(key.compareTo((root.key))>0)
+           return contains(root.rightnode,key);
+       else
+           return contains(root.leftnode,key);
+   }
+   //二叉搜索的函数
+    public Value search(Node root,Key key)
+    {
+        if(root==null)
+            return null;
+        if(key.compareTo(root.key)==0)
+            return root.value;
+        else if(key.compareTo(root.key)>0)
+            return search(root.rightnode,key);
+        else
+            return search(root.leftnode,key);
+    }
+
+}
+
